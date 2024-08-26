a0dfe55969389e9b6d1587fa32ab7a09da580246
poxuyi <xiroxamada87@gmail.com>
2024-08-26 16:56:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const item1 = document.querySelector('h1')
+const body = document.querySelector('body')
+const box = document.querySelector('.box')
+const image = document.querySelector('img')
+
+const name = document.querySelector('.box h1  span')
+
+const surname = document.querySelector('.box h2   span ')
+
+
+
+
+// const qu1 = prompt('Укажите цвет фона')
+// const qu2 = prompt('Укажите цвет бокса')
+// const qu3 = prompt('Укажите ширину бокса')
+const qu4 = prompt('Укажите ссылку на картинку')
+const qu5 = prompt('Укажите ваше имя')
+const qu6 = prompt('Укажите вашу фамилию')
+
+
+
+
+
+
+
+
+// body.style.backgroundColor = qu1
+// box.style.backgroundColor = qu2
+// box.style.width = qu3 + 'px'
+image.src = qu4
+name.innerText = qu5
+surname.innerText = qu6 
